b449755c5b2f7e1c5ae08c2d2ae9a0c7440cc592
gustavo.souza <gustavo.souza@4all.com>
2017-02-03 18:12:14 -0200

    add animation transitions

--- app/src/main/java/br/com/infoterras/bindapplication/ViewModel/ContentViewModel.java
+++ app/src/main/java/br/com/infoterras/bindapplication/ViewModel/ContentViewModel.java
@@ -5,14 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
-import android.util.Log;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
 import br.com.infoterras.bindapplication.BR;
