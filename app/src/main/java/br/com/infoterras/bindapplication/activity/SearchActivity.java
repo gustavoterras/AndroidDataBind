83dc909a3451c57931ce36b5076f9e9d3ffc9b5b
gustavo.souza <gustavo.souza@4all.com>
2017-02-08 13:59:27 -0200

    dismiss keyboard

--- app/src/main/java/br/com/infoterras/bindapplication/activity/SearchActivity.java
+++ app/src/main/java/br/com/infoterras/bindapplication/activity/SearchActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
